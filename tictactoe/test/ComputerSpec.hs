c442cf7ee141f77403972ac5cecaa293261038e8
Hana Lee <hanalee@post.harvard.edu>
2017-01-23 13:04:04 -0600

    Fix tests that were failing from type errors
    Some tests were commented out due to expecting monad Int instead of Int
    Resolved issue by running assertions through fixtures from test-fixtures

--- tictactoe/test/ComputerSpec.hs
+++ tictactoe/test/ComputerSpec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell, FlexibleInstances #-}
+
 module ComputerSpec where
 
 import Test.Hspec
@@ -6,6 +8,10 @@ import Rules ( GameData(Game) )
 import Board ( BoardData(Rows) )
 import Moves ( MovesData(GameState) )
 import UI ( MonadUI )
+import Control.Monad.TestFixture
+import Control.Monad.TestFixture.TH
+
+mkFixture "Fixture" [''MonadUI]
 
 main :: IO ()
 main = hspec spec
@@ -19,22 +25,22 @@ spec = do
   let nearEndGame = GameState [4, 0, 1, 7, 5, 3, 6, 8]
   let midGame = GameState [4, 0, 1, 7, 3, 6]
 
-
-
+  let fixture = def {
+      _readLine = return ""
+  }
 
   describe "makeMove" $ do
     it "makes the only possible move" $ do
-      shouldBe True True
-{-
       let game = Game board nearEndGame
-      shouldBe (makeMove computer game) 2
+      let move = unTestFixture (makeMove computer game) fixture
+      shouldBe move 2
 
     it "chooses the winning move out of three possible moves" $ do
       let game = Game board midGame
-      shouldBe (makeMove computer game) 5
+      let move = unTestFixture (makeMove computer game) fixture
+      shouldBe move 5
 
     it "can make a move at the beginning of a game" $ do
       let game = Game board newGame
-      shouldBe (elem (makeMove computer game) [0..8]) True
-
--}
+      let move = unTestFixture (makeMove computer game) fixture
+      shouldBe (elem move [0..8]) True
--- tictactoe/test/GameSpec.hs
+++ tictactoe/test/GameSpec.hs
@@ -13,7 +13,7 @@ import Rules
     )
 import Settings ( PlayerData(Players) )
 import Mocks ( MockPlayerData(MockPlayer) )
-import UI ( MonadUI )
+import UI ( MonadUI, readLine )
 import Control.Monad.TestFixture
 import Control.Monad.TestFixture.TH
 
@@ -31,38 +31,42 @@ spec = do
   let unfinishedGame = GameState [4, 2, 5, 1, 6]
   let players = Players MockPlayer MockPlayer
 
-  let fixture = def {
+  let writeFixture = def {
       _writeLine = \line -> tell line
+  }
+
+  let readFixture = def {
+      _readLine = return ""
   }
 
   describe "endGame" $ do
     it "gives result if Player One wins" $ do
-      let result = logTestFixture (endGame PlayerOneWin) fixture
+      let result = logTestFixture (endGame PlayerOneWin) writeFixture
       shouldBe result "Player One wins\n"
 
     it "gives result if Player Two wins" $ do
-      let result = logTestFixture (endGame PlayerTwoWin) fixture
+      let result = logTestFixture (endGame PlayerTwoWin) writeFixture
       shouldBe result "Player Two wins\n"
 
     it "gives result if draw" $ do
-      let result = logTestFixture (endGame Draw) fixture
+      let result = logTestFixture (endGame Draw) writeFixture
       shouldBe result "Game ends in a draw\n"
 
-{-
   describe "nextMove" $ do
     it "returns a move when next player is player one" $ do
       let game = Game board newGame
-      shouldBe (nextMove game players) 0
+      let move = unTestFixture (nextMove game players) readFixture 
+      shouldBe move 0
 
     it "returns a move when next player is player two" $ do
       let game = Game board unfinishedGame
-      shouldBe (nextMove game players) 0
--}
+      let move = unTestFixture (nextMove game players) readFixture 
+      shouldBe move 0 
 
   describe "runGame" $ do
     it "ends game if the game is finished" $ do
       let game = Game board finishedGame
-      let result = logTestFixture (runGame game players) fixture
+      let result = logTestFixture (runGame game players) writeFixture
       shouldBe result "Player Two wins\n"
 
     it "makes move and ends game if game is unfinished" $ do
@@ -72,5 +76,5 @@ spec = do
                        \ 3 | X | X \n\
                        \---+---+---\n\
                        \ X | 7 | 8 "
-      let result = logTestFixture (runGame game players) fixture
+      let result = logTestFixture (runGame game players) writeFixture
       shouldBe result ("\nTurn 6:\n" ++ gameString ++ "\nPlayer Two wins\n")
